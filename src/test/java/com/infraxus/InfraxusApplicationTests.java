44bd3cdcef459690be50ecad39542a6c0c794d2c
이재현 <ij184399@gmail.com>
2025-06-11 14:34:45 +0900

    프로젝트 세팅

---
+++ src/test/java/com/infraxus/InfraxusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infraxus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraxusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
