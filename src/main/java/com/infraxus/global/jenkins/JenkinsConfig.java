8734e622aff9f62d25e1caf1bc102dc0d485204a
ljhy07 <ij184399@gmail.com>
2025-07-11 17:07:07 +0900

    ADD : Jenkins Config

---
+++ src/main/java/com/infraxus/global/jenkins/JenkinsConfig.java
@@ -0,0 +1,28 @@
+package com.infraxus.global.jenkins;
+
+import com.offbytwo.jenkins.JenkinsServer;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Getter
+@Configuration
+public class JenkinsConfig {
+    @Value("${jenkins.url}")
+    private String url;
+
+    @Value("${jenkins.username}")
+    private String username;
+
+    @Value("${jenkins.token}")
+    private String token;
+
+    @Bean
+    public JenkinsServer jenkinsServer() throws URISyntaxException {
+        return new JenkinsServer(new URI(url), username, token);
+    }
+}
